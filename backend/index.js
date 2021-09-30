a9c338549906b267c22a63371ab35458917548e8
Asif Javed <90240025+Asifcreative@users.noreply.github.com>
2021-09-30 15:53:52 +0500

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const connetToMongo = require('./db');
+const express = require('express')
+
+connetToMongo();
+const app = express()
+const port = 5000
+
+app.use(express.json())
+
+//Available Routes
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
